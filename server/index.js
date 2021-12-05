17453c84631ba8a1e1e902e1cc281d3eecbeb8f1
Seongeuniii <kse8538@naver.com>
2021-12-05 13:56:38 +0900

    Join UI

--- server/index.js
+++ server/index.js
@@ -4,8 +4,20 @@ const http = require('http');
 
 const PORT = process.env.PORT || 5000;
 
+const router = require('./router')
+
 const app = express();
-const server = http.createServer(app);
+const server = http.createServer(app); // server 객체 생성
 const io = socketio(server);
 
-server.listen(PORT, () => console.log(`Server has started on port ${PORT}`));
+io.on('connection', (socket) => { // client와 연결되었을 때 발생
+  console.log('We have a new connetion!!!');
+  socket.on('disconnect', () => { // client와 연결해제되었을 때 발생
+    console.log('User had left!!!');
+  })
+});
+
+app.use(router);
+
+
+server.listen(PORT, () => console.log(`Server has started on port ${PORT}`)); // socket 연결을 기다림
