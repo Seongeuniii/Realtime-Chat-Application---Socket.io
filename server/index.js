8961cf88324d3586c1208694229eb2e911c8b67d
Seongeuniii <kse8538@naver.com>
2022-01-05 23:29:43 +0900

    error handling

--- server/index.js
+++ server/index.js
@@ -13,16 +13,18 @@ const io = socketio(server);
 io.on('connection', (socket) => { // client와 연결되었을 때 발생
   console.log('We have a new connetion!!!');
 
-  // socket.on('join', ({ name, room }) => {
-  //   console.log(name, room);
-  // })
+  socket.on('join', ({ name, room }, callback) => { // on, 이벤트 리스터로 전달된 데이터가 있을 때 실행된다.
+    const error = true;
+    if(error) { // 에러 핸들링
+      callback({ error: 'error' })
+    }
+  })
 
-  // socket.on('disconnect', () => { // client와 연결해제되었을 때 발생
-  //   console.log('User had left!!!');
-  // })
+  socket.on('disconnect', () => { // client와 연결해제되었을 때 발생
+    console.log('User had left!!!');
+  })
 });
 
 app.use(router);
-
 
 server.listen(PORT, () => console.log(`Server has started on port ${PORT}`)); // socket 연결을 기다림
