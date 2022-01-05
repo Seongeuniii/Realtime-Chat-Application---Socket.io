18315f76406ad4e41fe3f66f80fcd4a48e7696d6
Seongeuniii <kse8538@naver.com>
2022-01-06 00:30:51 +0900

    Make methods when users enter chat room

--- server/index.js
+++ server/index.js
@@ -2,6 +2,8 @@ const express = require('express');
 const socketio = require('socket.io');
 const http = require('http');
 
+const { addUser, removeUser, getUser, getUsersInRoom }  = require('./users.js')
+
 const PORT = process.env.PORT || 5000;
 
 const router = require('./router');
@@ -14,10 +16,24 @@ io.on('connection', (socket) => { // client와 연결되었을 때 발생
   console.log('We have a new connetion!!!');
 
   socket.on('join', ({ name, room }, callback) => { // on, 이벤트 리스터로 전달된 데이터가 있을 때 실행된다.
-    const error = true;
-    if(error) { // 에러 핸들링
-      callback({ error: 'error' })
-    }
+    const { error, user } = addUser({ id: socket.id, name, room });
+
+    if(error) return callback(error);
+
+    socket.emit('message', { user: 'admin', text: `${user.name}, welcome to the room ${user.room}` });
+    socket.broadcast.to(user.room).emit('message', { user: 'admin', text: `${user.name}, has joined!` });
+    
+    socket.join(user.room);
+
+    callback();
+  })
+
+  socket.on('sendMessage', (message, callback) => {
+    const user = getUser(socket.id);
+
+    io.to(user.room).emit('message', { user: user.name, text: message});
+
+    callback()
   })
 
   socket.on('disconnect', () => { // client와 연결해제되었을 때 발생
---
+++ server/users.js
@@ -0,0 +1,33 @@
+const users = [];
+
+const addUser = ({ id, name, room }) => {
+  // JavaScript Mastery = javascriptmastery 처럼 하나로 만들기
+  name = name.trim().toLowerCase()
+  room = room.trim().toLowerCase()
+
+  const existingUser = user.find((user) => user.room === room && user.name === name);
+
+  if(existingUser) {
+    return { error: 'Username is taken' };
+  }
+
+  const user = { id, name, room };
+
+  user.push(user);
+
+  return { user }
+}
+
+const removeUser = () => {
+  const index = users.findIndex((user) => user.id === id);
+
+  if(index !== -1) {
+    return users.splice(index, 1)[0];
+  }
+}
+
+const getUser = (id) => users.find((user) => user.id === id);
+
+const getUsersInRoom = (room) => users.filter((user) => user.room === room);
+
+module.exports = { addUser, removeUser, getUser, getUsersInRoom };
